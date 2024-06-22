dacaed522c93a3ba79be79211033434dcc801929
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-06-22 23:08:22 +0200

    render function from pixabay file

--- js/pixabay-api.js
+++ js/pixabay-api.js
@@ -1,18 +1,24 @@
-export function fetchGallery(q) {
+import axios from 'axios';
+
+export async function fetchGallery(q, currentPage) {
   const BASE_URL = 'https://pixabay.com';
   const END_POINT = '/api/';
+  const url = `${BASE_URL}${END_POINT}`;
 
-  const params = new URLSearchParams({
+  const params = {
     key: '44406929-b6512fae58ed0703418d850ba',
     q: q,
+    page: currentPage,
+    per_page: 15,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
-  });
+  };
 
-  const url = `${BASE_URL}${END_POINT}?${params}`;
-
-  return fetch(url)
-    .then(res => res.json())
-    .catch(err => console.log(err));
+  try {
+    const res = await axios.get(url, { params });
+    return res.data;
+  } catch {
+    console.log('Error');
+  }
 }
